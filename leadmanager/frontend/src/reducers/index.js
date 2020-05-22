ab6e65c3c5caeaebe1291b51a83ef6de3839dea7
niles87 <nbing19@gmail.com>
2020-05-21 20:23:04 -0700

    added login, register, and privateroutes components, added authentication routes and reducer with actions

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 import leads from "./leads";
 import errors from "./error";
 import messages from "./messages";
+import auth from "./auth";
 
-export default combineReducers({ leads, errors, messages });
+export default combineReducers({ leads, errors, messages, auth });
